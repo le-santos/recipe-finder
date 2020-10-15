61f4e1b608d5a088941ed2def01eadd5dc855cd5
le-santos <lequinterio@gmail.com>
2020-10-14 21:57:42 -0300

    changes menu styles

--- src/Components/Navigation/NavigationItem.js
+++ src/Components/Navigation/NavigationItem.js
@@ -2,8 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 const LinkStyled = styled.a`
-  padding: 1em 0.5em;
+  padding: 0.5em 1em;
   color: var(--color0);
+  &:hover {
+    color: var(--color1);
+    background-color: var(--color5);
+		cursor: pointer;
+	}
 `
 
 function NavigationItem(props) {
--- src/Components/SideDrawer/SideDrawer.js
+++ src/Components/SideDrawer/SideDrawer.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import CloseIcon from "../Icons/CloseIcon";
-import UtensilsIcon from "../Icons/UtensilsIcon";
 import Backdrop from "../UI/Backdrop";
 import NavigationList from "../Navigation/NavigationList";
+import Header from "../UI/Header";
 
 const SideDivStyled = styled.div`
   position: fixed;
@@ -14,7 +14,6 @@ const SideDivStyled = styled.div`
   max-width: 60%;
   background-color: var(--color2);
   z-index: 200;
-  padding: 1em 0.5em;
   transition: left 0.5s;
 `
 
@@ -25,16 +24,22 @@ function SideDrawer(props) {
       (props.isSideDrawerVisible) ? "0px" : "-500px"
     )
   }
-  
+ 
+  const headerStyle = {
+    backgroundColor: "var(--color2)",
+    color: "var(--color5)"
+  }
 
   return (
     <React.Fragment>
       <Backdrop clicked={props.closeSideDrawer} isBackdropVisible={props.isSideDrawerVisible}/>
       <SideDivStyled changeLeftPosition={moveDrawer}>
-        <UtensilsIcon size={"2x"} color={"var(--color5)"} />
-        <span style={{"float": "right", "fontSize": "2em", cursor: "pointer"}}>
-          <CloseIcon color={"var(--color5)"} size={"sm"} clicked={props.closeSideDrawer}/>
-        </span>
+        <Header customStyle={headerStyle}>
+          <h3>Recipes by category</h3>
+          <span style={{"float": "right", "fontSize": "2em", cursor: "pointer"}}>
+            <CloseIcon  size={"sm"} clicked={props.closeSideDrawer}/>
+          </span>
+        </Header>
         <NavigationList menuList={props.menuList} />
       </SideDivStyled>
     </React.Fragment>
