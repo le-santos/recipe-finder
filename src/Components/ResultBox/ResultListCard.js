a2578426d3b0fa44d37fde486ef8543382479174
le-santos <lequinterio@gmail.com>
2025-11-16 17:43:44 -0300

    feat: add option to save recipes to favorites in localStorage

--- src/Components/ResultBox/ResultListCard.js
+++ src/Components/ResultBox/ResultListCard.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "../UI/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 const DivCard = styled.div`
   margin: 1em;
@@ -13,18 +15,39 @@ const DivCard = styled.div`
 `;
 
 function RecipeListCard({ id, src, alt, recipeName, selectCard }) {
+  const addToFavorites = () => {
+    const newFavItem = { name: recipeName, id: id, src: src };
+    const storedFavorites = localStorage.getItem("favorites");
+    const storedRecipes = JSON.parse(storedFavorites)?.recipes || {};
+
+    storedRecipes[newFavItem.id] = newFavItem;
+
+    const jsonFav = { recipes: storedRecipes };
+    localStorage.setItem("favorites", JSON.stringify(jsonFav));
+    console.log("Added to favorites");
+  };
+
   return (
     <DivCard id={id}>
       <img src={src} alt={alt} width={"200px"} height={"auto"} />
       <h3>{recipeName}</h3>
-      <Button
-        type={"button"}
-        name={"Recipe Details"}
-        id={"details"}
-        clicked={selectCard}
-        color={"var(--color5)"}
-        hoverColor={"var(--color2)"}
-      />
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Button
+          type={"button"}
+          name={"Recipe Details"}
+          id={"details"}
+          clicked={selectCard}
+          color={"var(--color5)"}
+          hoverColor={"var(--color2)"}
+        />
+        <FontAwesomeIcon
+          icon={faHeart}
+          color={"var(--color4)"}
+          size={"lg"}
+          onClick={addToFavorites}
+          style={{ cursor: "pointer", alignSelf: "center" }}
+        />
+      </div>
     </DivCard>
   );
 }
